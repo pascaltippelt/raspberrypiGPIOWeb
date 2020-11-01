58f2b55dd89bb88add8747a4d94b86a3577d4723
Pascal Tippelt <2066297+pascaltippelt@users.noreply.github.com>
2020-11-01 15:07:10 +0100

    Update index.php

--- apache/gpio/index.php
+++ apache/gpio/index.php
@@ -1,80 +1,109 @@
 <?php
-	//print "Beginn";
-	$aNames = array();
+
+	define("ON", 0);
+	define("OFF", 1);
+
+	class cRelay{
+		public $iPin 			= -1;
+		public $iState 			= OFF;
+		public $sName 			= "No Name";
+		public $iTimerOn 		= 420;
+		public $iTimerOff 		= 480;	
+		public $iTimerActive	= 0;
+
+		public function __construct($iPin, $iState, $sName, $iTimerOn, $iTimerOff) {
+			$this->iPin = $iPin;
+			$this->iState = $iState;
+			$this->sName = $sName;
+			$this->iTimerOn = $iTimerOn;
+			$this->iTimerOff = $iTimerOff;
+			$this->iTimerActive = 0;
+			
+		}
+	}
 	
-	if (file_exists('relay_config.bin')) {
+	//Konfiguration laden oder erstellen
+	//$aConfig = array(array());
+	if (file_exists('/var/www/html/config.bin')) {
 		//config file exists, read it out
-		$confFile = fopen('relay_config.bin', 'r') or die('Konnte config file nicht laden');
-		$aNames = unserialize(fread($confFile,filesize('relay_config.bin')));
-		fclose($confFile);
+		$fConfigFile = fopen('/var/www/html/config.bin', 'r') or die('Konnte config.bin nicht laden\r');
+        $aConfig = unserialize(fread($fConfigFile, filesize('/var/www/html/config.bin')));
+        fclose($fConfigFile);
+		//echo "Success";
 		
 	} else {
 		//config file has to be created and initialized
-		$aNames = array('12V Nr. 1', '12V Nr. 2', '12V Nr. 3', '12V Nr. 4', '230V Nr. 1', '230V Nr. 2', '230V Nr. 3',  '230V Nr. 4');
-		$confFile = fopen('relay_config.bin', 'w') or die('Konnte config file nicht laden');
-		fwrite($confFile, serialize($aNames));
-		fclose($confFile);
-	}
-
-	//Deklarieren des Relaisboard
-	class cRelayBoard {
-		private	$aWpPins = array(0,2,3,12,13,14,21,22);
-		public	$aStates = array(-1,-1,-1,-1,-1,-1,-1,-1);
-
-		function fnSetState($nr, $state) {
-			shell_exec('gpio write ' . $this->aWpPins[$nr] . ' ' . $state);
-			//print 'gpio write ' . $this->aWpPins[$nr] . ' ' . $state;
-		}
-
-		function fnGetState($nr) {
-			$this->aStates[$nr] = shell_exec('gpio read ' . $this->aWpPins[$nr]);
-			return $this->aStates[$nr];
-		}
-
-		function __construct() {
-			for ($i = 0; $i <=7; $i++) {
-				shell_exec('gpio mode ' . $this->aWpPins[$i] . ' OUT');
-				//print $this->aWpPins[$i];
-			}
-		}
-	}
-
-	//Neues Relayboard
-	$MyRelay = new cRelayBoard;
-
+		//Inside-Array (PIN,STATE,NAME,TIMER_ON,TIMER_OFF)
+		//PIN:		wiredpi-pin
+		//STATE:	-1 [illegal], 0 [off], 1 [on], 2 [automatic_off], 3 [automatic_on]
+		//array(0,2,3,12,13,14,21,22);
+		$aConfig = array(new cRelay(0,false,"New Relay 1",420,480), new cRelay(2,false,"New Relay 2",420,480), new cRelay(3,false,"New Relay 3",420,480), new cRelay(12,false,"New Relay 4",420,480), new cRelay(13,false,"New Relay 5",420,480), new cRelay(14,false,"New Relay 6",420,480), new cRelay(21,false,"New Relay 7",420,480), new cRelay(22,false,"New Relay 8",420,480));
+		$fConfigFile = fopen('/var/www/html/config.bin', 'w') or die('Konnte config.bin nicht laden\r');
+		fWrite($fConfigFile, serialize($aConfig));
+        fclose($fConfigFile);
+	}	
+	
 	//Buttons
-	$setNr = $_GET['setNr'];
-	$setState = $_GET['setState'];
-
-	//Eventuell Zustände setzen
-
+	$setNr 			= $_GET['setNr'];
+	$setState 		= $_GET['setState'];
+	$setMode 		= $_GET['setMode'];
+	$changeID 		= $_GET['selectbox_name'];
+	$changeIDTimer 	= $_GET['selectbox_timer'];
+	$changeNewName 	= $_GET['text_name'];
+	$changeTimerOn 	= $_GET['timer_on'];
+	$changeTimerOff = $_GET['timer_off'];
+	
+	//Stati setzen
 	if (($setNr === NULL) or ($setState === NULL)) {
 		//print 'no';
 	} else {
 		//print 'yes';
-		$MyRelay->fnSetState($setNr,$setState);
+		shell_exec('gpio write ' . $aConfig[$setNr]->iPin . ' ' . $setState);
 	}
-
-	//Formular zum Umbenennen
-	//?selectbox_name=4&text_name=&button_name=Senden
-	$changeID = $_GET['selectbox_name'];
-	$changeNewName = $_GET['text_name'];
 	
-	if (($changeID === NULL) or ($changeNewName === NULL)) {
-		
+	//Modi setzen
+	if (($setNr === NULL) or ($setMode === NULL)) {
+		//
 	} else {
-		$aNames[$changeID] = $changeNewName;
-		$confFile = fopen('relay_config.bin', 'w') or die('Konnte config file nicht laden');
-		fwrite($confFile, serialize($aNames));
-		fclose($confFile);
+		$aConfig[$setNr]->iTimerActive = $setMode;
+		$fConfigFile = fopen('/var/www/html/config.bin', 'w') or die('Konnte config.bin nicht laden\r');
+		fWrite($fConfigFile, serialize($aConfig));
+        fclose($fConfigFile);
 	}
-
-	//Alle Zustände abfragen
+	
+	//Change Name
+	if (($changeID === NULL) or ($changeNewName === NULL)) {
+		//Nothing
+	} else {
+		$aConfig[$changeID]->sName = $changeNewName;
+		$fConfigFile = fopen('/var/www/html/config.bin', 'w') or die('Konnte config.bin nicht laden\r');
+		fWrite($fConfigFile, serialize($aConfig));
+        fclose($fConfigFile);
+	}
+	
+	//Change Timers
+	//echo $changeID . " " . $changeTimerOn . " " . $changeTimerOff;
+	if (isset($changeIDTimer) and isset($changeTimerOn) and isset($changeTimerOff)) {
+		//echo $changeIDTimer . " " . $changeTimerOn . " " . $changeTimerOff;
+		$aTimerOn = explode(':',$changeTimerOn);
+		$aTimerOff = explode(':',$changeTimerOff);
+		$aConfig[$changeIDTimer]->iTimerOn = $aTimerOn[0]*60 + $aTimerOn[1];
+		$aConfig[$changeIDTimer]->iTimerOff = $aTimerOff[0]*60 + $aTimerOff[1];
+		$fConfigFile = fopen('/var/www/html/config.bin', 'w') or die('Konnte config.bin nicht laden\r');
+		fWrite($fConfigFile, serialize($aConfig));
+        fclose($fConfigFile);
+	} 
+	
+	//Stati abfragen
 	for ($i = 0; $i <=7; $i++) {
-		$MyRelay->fnGetState($i);
+		$aConfig[$i]->iState = shell_exec('gpio read ' . $aConfig[$i]->iPin);
 	}
 
+
+
+	
 ?>
+
 <!doctype html>
 <!--[if lt IE 7]> <html class="ie6 oldie"> <![endif]-->
 <!--[if IE 7]>    <html class="ie7 oldie"> <![endif]-->
@@ -84,7 +113,8 @@
 <!--<![endif]-->
 <head>
 <meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
+<!--<meta name="viewport" content="width=device-width, initial-scale=1">-->
+<meta http-equiv="refresh" content="10, url=index.php">
 <title>Raspberry Pi Relaisboard</title>
 <link href="boilerplate.css" rel="stylesheet" type="text/css">
 <link href="mobil.css" rel="stylesheet" type="text/css">
@@ -106,47 +136,60 @@ table, th, td {
 		<table>
 			<tbody>  
 			<tr>
-				<td width="50px"><b>Status</b></td>
+				<td ><b>Status</b></td>
 				<td width="80px"><b>Name</b></td>
-				<td width="100px"><b>Aktionen</b></td>
-				<td	width="80px"><b>Timer 1 ein</b></td>
-				<td width="80px"><b>Timer 1 aus</b></td>
+				<td width="150px"><b><center>Aktionen</center></b></td>
+				<td	width="95px"><b><center>Timer</center></b></td>
 			</tr>
 				<?php
 					for ($i = 0; $i <=7; $i++) {
 						if ($i > 0) {
 							echo "\t\t\t";
 						}
-						
+
 						echo '<tr>';
 							//1. Spalte | Status
-							echo '<td>';
+							echo '<td><center>';
 								echo '<img src="img/';
-								if ( $MyRelay->aStates[$i] == 1 ) {
-									echo 'red';
-								} else {
+								if ( $aConfig[$i]->iState == ON ) {
 									echo 'green';
+								} else {
+									echo 'red';
 								}
 								echo '.png">';
-							echo '</td>';
-							
+							echo '</center></td>';
+
 							//2. Spalte | Name
 							echo '<td>';
-								echo $aNames[$i];
+								echo $aConfig[$i]->sName;
 							echo '</td>';
-							
+
 							//3. Spalte | Aktion
-							echo '<td>';
-							echo '<a href="?setNr=' . $i . '&amp;setState=';
-								if ( $MyRelay->aStates[$i] == 1 ) {
-									echo '0"><button>einschalten';
+							echo '<td><center>';
+								if ($aConfig[$i]->iTimerActive == 0) {
+									if ( $aConfig[$i]->iState == OFF ) {
+										echo '<a href="?setNr=' . $i . '&amp;setState=0"><button>einschalten';
+									} else {
+										echo '<a href="?setNr=' . $i . '&amp;setState=1"><button>ausschalten';
+									}
 								} else {
-									echo '1"><button>ausschalten';
+									echo 'An von ' . intDiv($aConfig[$i]->iTimerOn,60) . ":" . sprintf('%02d',$aConfig[$i]->iTimerOn%60) . " bis " . intdiv($aConfig[$i]->iTimerOff,60) . ":" . sprintf('%02d',$aConfig[$i]->iTimerOff%60) . " Uhr";
+									//
 								}
-								echo '</button></a>';
+								echo '</button></center></a>';
 							echo '</td>';
-							echo '<td><b></b></td><td><b></b></td>';
-						echo '<tr>';						
+
+							//4.palte Timer							
+							echo '<td><center>';
+								if ($aConfig[$i]->iTimerActive == 1) {
+									echo '<a href="?setNr=' . $i . '&amp;setMode=0"><button>deaktivieren';
+								} else {
+									echo '<a href="?setNr=' . $i . '&amp;setMode=1"><button>aktivieren';
+								}		
+							echo '</button></a></center></td>';
+							
+
+						echo '<tr>';
 						echo "\r\n";
 					}
 				?>
@@ -154,17 +197,36 @@ table, th, td {
 		</table>
 	</div>
 	<br>
+	<div align="center">
+		<h2>Einstellungen</h2>
+	</div>
 	<div>
 		<form>
 			<select name="selectbox_name">
 				<?php 
 					for ($i = 0; $i <=7; $i++) {
-						echo '<option value="' . $i . '">' . $aNames[$i] . '</option>';
+						echo '<option value="' . $i . '">' . $aConfig[$i]->sName . '</option>';
 					}
 				?>				
 			</select>
 			<label> --> </label>
 			<input type="text" name="text_name" placeholder="neuer Name">
+			<input type="submit" name="button_name" value="Senden">
+		</form>
+	</div>
+	<br>
+	<div>
+		<form>
+			<select name="selectbox_timer">
+				<?php 
+					for ($i = 0; $i <=7; $i++) {
+						echo '<option value="' . $i . '">' . $aConfig[$i]->sName . '</option>';
+					}
+				?>				
+			</select>
+			<label> --> </label>			
+			<input id="appt-time" type="time" name="timer_on">
+			<input id="appt-time" type="time" name="timer_off">
 			<input type="submit" name="button_name" value="Senden">
 		</form>
 	</div>
